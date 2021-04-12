52683cbde214b1799c56caaa419910a1cea2c048
Gowtham Rao <gowthamrao@gmail.com>
2021-04-12 08:57:30 -0400

    Unzipped files

---
+++ extras/CodeToRun.R
@@ -0,0 +1,62 @@
+library(SkeletonCohortDiagnosticsStudy)
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores()
+
+# The folder where the study intermediate and result files will be written:
+outputFolder <- "s:/SkeletonCohortDiagnosticsStudy"
+
+# Details for connecting to the server:
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                server = Sys.getenv("PDW_SERVER"),
+                                                                user = NULL,
+                                                                password = NULL,
+                                                                port = Sys.getenv("PDW_PORT"))
+
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
+
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- "scratch.dbo"
+cohortTable <- "rao_skeleton"
+
+# Some meta-information that will be used by the export function:
+databaseId <- "Synpuf"
+databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
+databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
+
+# For Oracle: define a schema that can be used to emulate temp tables:
+oracleTempSchema <- NULL
+
+runCohortDiagnostics(packageName = "SkeletonCohortDiagnosticsStudy",
+                     connectionDetails = connectionDetails,
+                     cdmDatabaseSchema = cdmDatabaseSchema,
+                     cohortDatabaseSchema = cohortDatabaseSchema,
+                     cohortTable = cohortTable,
+                     oracleTempSchema = oracleTempSchema,
+                     outputFolder = outputFolder,
+                     databaseId = databaseId,
+                     databaseName = databaseName,
+                     databaseDescription = databaseDescription,
+                     runCohortCharacterization = TRUE,
+                     runCohortOverlap = TRUE,
+                     runOrphanConcepts = TRUE,
+                     runVisitContext = TRUE,
+                     runIncludedSourceConcepts = TRUE,
+                     runTimeDistributions = TRUE,
+                     runTemporalCohortCharacterization = TRUE,
+                     runBreakdownIndexEvents = TRUE,
+                     runInclusionStatistics = TRUE,
+                     runIncidenceRates = TRUE,
+                     createCohorts = TRUE,
+                     minCellCount = 0)
+
+CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = outputFolder)
+
+CohortDiagnostics::launchDiagnosticsExplorer(dataFolder = outputFolder)
+
+
+# Upload the results to the OHDSI SFTP server:
+privateKeyFileName <- ""
+userName <- ""
+SkeletonCohortDiagnosticsStudy::uploadResults(outputFolder, privateKeyFileName, userName)
---
+++ extras/PackageMaintenance.R
@@ -0,0 +1,44 @@
+# Copyright 2021 Observational Health Data Sciences and Informatics
+#
+# This file is part of SkeletonCohortDiagnosticsStudy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Format and check code ---------------------------------------------------
+OhdsiRTools::formatRFolder()
+OhdsiRTools::checkUsagePackage("SkeletonCohortDiagnosticsStudy")
+OhdsiRTools::updateCopyrightYearFolder()
+devtools::spell_check()
+
+# Create manual -----------------------------------------------------------
+unlink("extras/UsingSkeletonPackage.pdf")
+shell("R CMD Rd2pdf ./ --output=extras/UsingSkeletonPackage.pdf")
+
+# Create vignettes ---------------------------------------------------------
+rmarkdown::render("vignettes/UsingSkeletonPackage.Rmd",
+                  output_file = "../inst/doc/UsingSkeletonPackage.pdf",
+                  rmarkdown::pdf_document(latex_engine = "pdflatex",
+                                          toc = TRUE,
+                                          number_sections = TRUE))
+
+# Insert cohort definitions from ATLAS into package -----------------------
+ROhdsiWebApi::insertCohortDefinitionSetInPackage(fileName = "CohortsToCreate.csv",
+                                                 baseUrl = Sys.getenv("baseUrl"),
+                                                 insertTableSql = TRUE,
+                                                 insertCohortCreationR = TRUE,
+                                                 generateStats = TRUE,
+                                                 packageName = "SkeletonCohortDiagnosticsStudy")
+
+
+# Store environment in which the study was executed -----------------------
+OhdsiRTools::createRenvLockFile("SkeletonCohortDiagnosticsStudy")
