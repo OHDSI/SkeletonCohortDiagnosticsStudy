c2f338f9c405845bd6d4879265cb5a6adc927db4
Gowtham Rao <rao@ohdsi.org>
2021-10-12 05:58:22 -0400

    Updated renv
    based on discussion here https://github.com/OHDSI/Hydra/pull/31#issuecomment-940660680
    
    + package maintenance

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -24,11 +24,12 @@ unlink("extras/UsingSkeletonPackage.pdf")
 shell("R CMD Rd2pdf ./ --output=extras/UsingSkeletonPackage.pdf")
 
 # Store environment in which the study was executed -----------------------
-# OhdsiRTools::createRenvLockFile(rootPackage = "SkeletonCohortDiagnosticsStudy",
-#                                 additionalRequiredPackages = c('keyring', "checkmate","DatabaseConnector","clock","dplyr","DT",
-#                                                                "ggplot2","ggiraph","gtable","htmltools","lubridate",
-#                                                                "pool","purrr","scales","shiny","shinydashboard","shinyWidgets",
-#                                                                "stringr","SqlRender","tidyr", "plyr", "CirceR"))
+OhdsiRTools::createRenvLockFile(rootPackage = "SkeletonCohortDiagnosticsStudy",
+                                additionalRequiredPackages = c('keyring', "checkmate","DatabaseConnector","clock","dplyr","DT",
+                                                               "ggplot2","ggiraph","gtable","htmltools","lubridate",
+                                                               "pool","purrr","scales","shiny","shinydashboard","shinyWidgets",
+                                                               "stringr","SqlRender","tidyr", "plyr", "CirceR"),
+                                includeRootPackage = FALSE)
 
 
 
